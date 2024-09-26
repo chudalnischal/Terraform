bc991175ad261b54a9248d88e985fb7a7b06e295
Nischal CHudal <chudalnischal7@gmail.com>
2024-09-26 01:20:51 -0400

    updating

--- ec2.tf
+++ ec2.tf
@@ -5,8 +5,7 @@ provider "aws" { # this will show the providers details
 resource "aws_instance" "machine" { #resource type name and what i am going to call
     ami = "ami-0c6d358ee9e264ff1" # ami number as it is free tier
     instance_type = "t2.micro" # the type of instance 
-
-    tags { #tags name 
+    tags = { #tags name 
         Name = "terraform-first-ec2" # creating the name of the instances 
     }
 } 
