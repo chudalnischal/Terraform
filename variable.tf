731330394e000faa9b081bfb689f04608d13be20
Nischal CHudal <chudalnischal7@gmail.com>
2024-12-15 14:19:44 -0500

    variables in terraform

--- variable.tf
+++ variable.tf
@@ -43,18 +43,3 @@ variable "object_example" {
 #use a new type of expression called an interpolation, which has the
 #following syntax:
  #      "${}"
-
-
-
-###### OUTPUT VARIABLES
-
-# instead of having to manually poke around the ec2 console to find out the ipaddress of your server
-# we can find out by using output variable
-
-output "ip_address_ec2" {
-    value = aws_instance.machine.machine.public_ip 
-    description = "this will return the public ip address of your instance"
-}
-#output variable show after running the terraform apply 
-
-# we can use terraform output command to list all the outputs without applying chanegs
